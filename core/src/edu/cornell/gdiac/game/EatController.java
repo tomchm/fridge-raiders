7968a6de5dc2655c49fd868ba6610c920d8332ff
salelder <sse34@cornell.edu>
2017-03-12 15:42:16 -0400

    Started stubbing in controllers.

---
+++ core/src/edu/cornell/gdiac/game/EatController.java
@@ -0,0 +1,8 @@
+package edu.cornell.gdiac.game;
+
+/**
+ * Created by Sal on 3/12/2017.
+ */
+public class EatController {
+    public void eat() {}
+}
---
+++ core/src/edu/cornell/gdiac/game/GrabController.java
@@ -0,0 +1,8 @@
+package edu.cornell.gdiac.game;
+
+/**
+ * Created by Sal on 3/12/2017.
+ */
+public class GrabController {
+    public void grab() {}
+}
