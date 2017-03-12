39295e6560fc19b9c7871f64f33783a5bf4761e9
salelder <sse34@cornell.edu>
2017-03-12 15:27:48 -0400

    Added a furniture model. Pretty simple at the moment, but it may even stay that way.

---
+++ core/src/edu/cornell/gdiac/game/model/FurnitureModel.java
@@ -0,0 +1,38 @@
+package edu.cornell.gdiac.game.model;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.physics.box2d.*;
+import edu.cornell.gdiac.game.GameCanvas;
+
+/**
+ * Created by Sal on 3/12/2017.
+ */
+public class FurnitureModel extends GameObject {
+    public FurnitureModel(float x, float y, float width, float height, float theta, String tag){
+        bodyDef = new BodyDef();
+        bodyDef.active = true;
+        bodyDef.fixedRotation = true;
+        bodyDef.linearDamping = 0.5f;
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.awake  = true;
+        bodyDef.allowSleep = true;
+        bodyDef.position.set(x,y);
+        bodyDef.angle = theta;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(0.5f*width, 0.5f*height);
+        fixtureDef = new FixtureDef();
+        fixtureDef.density = 1.0f;
+        fixtureDef.shape = shape;
+
+        this.tag = tag;
+    }
+
+
+    @Override
+    public void draw(GameCanvas canvas) {
+        if(texture != null){
+            canvas.draw(texture, Color.WHITE,origin.x,origin.y,body.getPosition().x*drawScale.x,body.getPosition().y*drawScale.x,0f,imageScale.x,imageScale.y);
+        }
+    }
+}
