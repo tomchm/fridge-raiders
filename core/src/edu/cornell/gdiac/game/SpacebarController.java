ba1ccd75771dd3dab9358151404ce4ea9fb0966f
salelder <sse34@cornell.edu>
2017-03-19 17:48:31 -0400

    Fixed a dumb mistake. Added a splashscreen.

--- core/src/edu/cornell/gdiac/game/SpacebarController.java
+++ core/src/edu/cornell/gdiac/game/SpacebarController.java
@@ -14,6 +14,8 @@ import edu.cornell.gdiac.util.PooledList;
 /** Used for all button-interactions. At the moment, grabbing/releasing furniture,
  *  opening/shutting doors, and eating food. */
 public class SpacebarController implements RayCastCallback {
+    private static final float MAX_DIST = 2f;
+
     private HingeController hingeController;
     private EatController eatController;
     private GrabController grabController;
@@ -68,7 +70,7 @@ public class SpacebarController implements RayCastCallback {
             // otherwise we have found an interactible gameobject!
             // close enough, and a proper class!
             float dist2 = player.getBody().getPosition().sub(candidateIntersections[k]).len2();
-            if (dist2 < bestdist2) {
+            if (dist2 < bestdist2 && dist2 < MAX_DIST*MAX_DIST) {
                 bestdist2 = dist2;
                 closest = gob;
             }
@@ -85,7 +87,7 @@ public class SpacebarController implements RayCastCallback {
                     && gob.getClass() != FurnitureModel.class
                     && gob.getClass() != DoorModel.class) continue;
             float dist2 = player.getBody().getPosition().sub(gob.getBody().getPosition()).len2();
-            if (dist2 < bestdist2) {
+            if (dist2 < bestdist2 && dist2 < MAX_DIST*MAX_DIST) {
                 bestdist2 = dist2;
                 closest = gob;
             }
