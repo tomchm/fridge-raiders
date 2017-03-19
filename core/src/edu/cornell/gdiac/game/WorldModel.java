8417d523e8bc5ba2c0fa59e584ea9e66e66e33ac
vanya <iz44@cornell.edu>
2017-03-19 17:50:42 -0400

    Fixed removing game objects

--- core/src/edu/cornell/gdiac/game/WorldModel.java
+++ core/src/edu/cornell/gdiac/game/WorldModel.java
@@ -150,6 +150,7 @@ public class WorldModel {
     }
 
     public void removeGameObject(GameObject gameObject) {
+        gameObject.deactivate(world);
         gameObjects.remove(gameObject);
     }
 
