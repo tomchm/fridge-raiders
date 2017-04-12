c46c7579b3326d49bab9aea4b6206e41a69a4194
Sean Viswanathan <sv287@cornell.edu>
2017-04-11 23:09:59 -0400

    delay and centered text

--- core/src/edu/cornell/gdiac/game/gui/TextGUIModel.java
+++ core/src/edu/cornell/gdiac/game/gui/TextGUIModel.java
@@ -24,30 +24,50 @@ public class TextGUIModel extends GUIModel{
     private boolean isFirstStage;
     private WorldModel worldModel;
     private String message;
-
+    private int messageStep;
+    private int countdown;
+    private boolean didCountdown = false;
 
     public TextGUIModel(WorldModel worldModel){
         tags = new String[] {"gothic72"};
         guiTag = "TextGUI";
         isFirstStage = true;
         this.worldModel = worldModel;
+        this.messageStep = -1;
         this.message = "";
+        countdown = 0;
+
     }
 
     public void update(float dt){
+        System.out.println(countdown);
+        if(countdown > 0){
+            countdown -= 1;
+            didCountdown = true;
+            if(countdown == 0){
+                this.message = "";
+            }
+        }
+
         if(worldModel.getPlayer().isSecondStage()){
             this.isFirstStage = false;
             this.message = "";
         }
-        if(isFirstStage && worldModel.getPlayer().canEatDesser()){
+        if(isFirstStage && worldModel.getPlayer().canEatDesser() && !didCountdown){
             this.message = "DESSERT UNLOCKED";
+            this.messageStep = 1;
+            this.countdown += 300;
         }
         else{
             if((!isFirstStage) && worldModel.hasExited()){
                 this.message = "LEVEL COMPLETE!";
+                this.messageStep = 2;
+                this.countdown += 6000;
             }
-            else if( (!isFirstStage) && worldModel.getPlayer().getShotsRemaining() < 1){
-                this.message = "GAME OVER!";
+            else if( (!isFirstStage) && worldModel.getPlayer().getShotsRemaining() == 0){
+                this.message = "   GAME OVER! \nCLICK TO RESTART";
+                this.messageStep = 3;
+                this.countdown += 6000;
             }
         }
 
@@ -56,16 +76,37 @@ public class TextGUIModel extends GUIModel{
     public void draw(GameCanvas canvas){
         float x = origin.x*GameObject.getDrawScale().x;
         float y = origin.y*GameObject.getDrawScale().y;
-        if(this.message != "") {
             FontAsset font = (FontAsset) assetMap.get("gothic72");
             if(font != null) {
-                BitmapFont bf = font.getFont();
-                bf.setColor(Color.BLACK);
-                canvas.drawText(this.message, font.getFont(), x-270+2, y+230-2);
-                bf.setColor(Color.YELLOW);
-                canvas.drawText(this.message, font.getFont(), x-270, y+230);
+                if(this.messageStep == 1) {
+                    BitmapFont bf = font.getFont();
+                    bf.setColor(Color.BLACK);
+                    canvas.drawText(this.message, font.getFont(), x - 320 + 2, y + 320 - 2);
+                    bf.setColor(Color.YELLOW);
+                    canvas.drawText(this.message, font.getFont(), x - 320, y + 320);
+                }
+                else if(this.messageStep == 2) {
+                    BitmapFont bf = font.getFont();
+                    bf.setColor(Color.BLACK);
+                    canvas.drawText(this.message, font.getFont(), x - 285 + 2, y + 320 - 2);
+                    bf.setColor(Color.YELLOW);
+                    canvas.drawText(this.message, font.getFont(), x - 285, y + 320);
+                }
+                else if(this.messageStep == 3) {
+                    BitmapFont bf = font.getFont();
+                    bf.setColor(Color.BLACK);
+                    canvas.drawText(this.message, font.getFont(), x - 280 + 2, y + 320 - 2);
+                    bf.setColor(Color.YELLOW);
+                    canvas.drawText(this.message, font.getFont(), x - 280, y + 320);
+                }
+                else {
+                    BitmapFont bf = font.getFont();
+                    bf.setColor(Color.BLACK);
+                    canvas.drawText(this.message, font.getFont(), x - 270 + 2, y + 230 - 2);
+                    bf.setColor(Color.YELLOW);
+                    canvas.drawText(this.message, font.getFont(), x - 270, y + 230);
+                }
             }
-        }
 
     }
 }
