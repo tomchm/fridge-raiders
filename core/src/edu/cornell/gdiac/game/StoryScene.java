f08ed6abd62d9bf4b5cb0968e7af07dc2093f635
salelder <sse34@cornell.edu>
2017-05-08 22:57:55 -0400

    Working on cutscenes

--- core/src/edu/cornell/gdiac/game/StoryScene.java
+++ core/src/edu/cornell/gdiac/game/StoryScene.java
@@ -3,6 +3,7 @@ package edu.cornell.gdiac.game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Queue;
 import edu.cornell.gdiac.game.asset.Asset;
 import edu.cornell.gdiac.game.asset.AssetLoader;
@@ -15,8 +16,8 @@ import edu.cornell.gdiac.util.SoundController;
  * Created by Sal on 5/7/2017.
  */
 public class StoryScene implements Screen {
-    private static float IMAGE_TIME = 4f;
-    private static float TEXT_TIME = 3f;
+    private static float IMAGE_TIME = 3f;
+    private static float TEXT_TIME = 5f;
     private static float FADE_TIME = 1f;
 
     public enum Level { APARTMENT_1, APARTMENT_2, APARTMENT_3, CLUB_1, CLUB_2, CLUB_3, MANSION_1, MANSION_2, MANSION_3 };
@@ -64,13 +65,21 @@ public class StoryScene implements Screen {
         gradient = ((ImageAsset)AssetLoader.getInstance().getAsset("gradient")).getTexture().getTexture();
 
         switch (levelCode) {
-            case 101:
+            case 100:
                 add("cutscene1");
-                storyText = "I've got more Chins than a Chinese phonebook.";
+                storyText = "Some are born to love. Others, to pray. I was born\nto break into people's houses and eat all their food.";
+                break;
+            case 101:
+                add("cutscene2");
+                storyText = "The rain comes down and the moon shimmers overhead.\nI must eat.";
+                break;
+            case 102:
+                add("cutscene3");
+                storyText = "I awake screaming and in a cold sweat. I can't shake the image\nof flat, off-brand soda.";
                 break;
             default:
                 add("cutscene1");
-                storyText = "I've got more Chins than a Chinese phonebook.";
+                storyText = "The rain comes down and the moon shimmers overhead.\nI must eat.";
                 break;
         }
 
@@ -94,9 +103,6 @@ public class StoryScene implements Screen {
         if (imgTime < FADE_TIME) {
             tint.r = tint.g = tint.b = imgTime / FADE_TIME;
         }
-        else if (imgTime > (IMAGE_TIME - FADE_TIME)) {
-            tint.r = tint.g = tint.b = (IMAGE_TIME - imgTime) / FADE_TIME;
-        }
 
         numChars = Math.min((int)(storyText.length() * sceneTime / TEXT_TIME), storyText.length());
 
@@ -104,17 +110,25 @@ public class StoryScene implements Screen {
 
         canvas.draw(currentImg, tint, -640, -360, 1280, 720);
         canvas.draw(gradient, -640, -360);
-        canvas.drawText("Space to skip", ((FontAsset)AssetLoader.getInstance().getAsset("gothic32")).getFont(), -600, 300);
-        canvas.drawText(storyText.substring(0,numChars), ((FontAsset)AssetLoader.getInstance().getAsset("typewriter")).getFont(), -600, -300);
+        //canvas.drawText("Space to skip", ((FontAsset)AssetLoader.getInstance().getAsset("gothic32")).getFont(), -600, 300);
+        canvas.drawText(storyText.substring(0,numChars), ((FontAsset)AssetLoader.getInstance().getAsset("typewriter")).getFont(), -600, -270);
 
         if (imgTime > IMAGE_TIME && imgQueue.size > 0) {
             imgTime = 0f;
             currentImg = imgQueue.removeFirst();
         }
 
+        if (sceneTime > TEXT_TIME) {
+            ImageAsset play = (ImageAsset)AssetLoader.getInstance().getAsset("playButton");
+            if(play != null){
+                Color tint = (true ? Color.GRAY: Color.WHITE);
+                float xx = 440f+5f* MathUtils.sin(sceneTime*0.1f*60f);
+                canvas.draw(play.getTexture(), tint, play.getOrigin().x, play.getOrigin().y, xx, -240f, 0, 1f, 1f);
+            }
+        }
+
         InputController.getInstance().readInput();
-        if (InputController.getInstance().didSkip()
-                || (imgTime > IMAGE_TIME && imgQueue.size == 0)) {
+        if (InputController.getInstance().didSkip()) {
             listener.exitScreen(this, levelCode);
         }
 
